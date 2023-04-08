379ce9166efc4bb70ee4975f6b8ba99d6c95c4f2
Ankit <ankit.engr11@gmail.com>
2023-04-09 01:01:05 +0530

    admin added

---
+++ admin/src/components/auth/login-form.tsx
@@ -0,0 +1,110 @@
+import Button from '@/components/ui/button';
+import Input from '@/components/ui/input';
+import PasswordInput from '@/components/ui/password-input';
+import { useTranslation } from 'next-i18next';
+import * as yup from 'yup';
+import Link from '@/components/ui/link';
+import Form from '@/components/ui/forms/form';
+import { Routes } from '@/config/routes';
+import { useLogin } from '@/data/user';
+import type { LoginInput } from '@/types';
+import { useState } from 'react';
+import Alert from '@/components/ui/alert';
+import Router from 'next/router';
+import {
+  allowedRoles,
+  hasAccess,
+  setAuthCredentials,
+} from '@/utils/auth-utils';
+
+const loginFormSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('form:error-email-format')
+    .required('form:error-email-required'),
+  password: yup.string().required('form:error-password-required'),
+});
+
+const LoginForm = () => {
+  const { t } = useTranslation();
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const { mutate: login, isLoading, error } = useLogin();
+
+  function onSubmit({ email, password }: LoginInput) {
+    login(
+      {
+        email,
+        password,
+      },
+      {
+        onSuccess: (data) => {
+          if (data?.token) {
+            if (hasAccess(allowedRoles, data?.permissions)) {
+              setAuthCredentials(data?.token, data?.permissions);
+              Router.push(Routes.dashboard);
+              return;
+            }
+            setErrorMessage('form:error-enough-permission');
+          } else {
+            setErrorMessage('form:error-credential-wrong');
+          }
+        },
+        onError: () => { },
+      }
+    );
+  }
+
+  return (
+    <>
+      <Form<LoginInput> validationSchema={loginFormSchema} onSubmit={onSubmit}>
+        {({ register, formState: { errors } }) => (
+          <>
+            <Input
+              label={t('form:input-label-email')}
+              {...register('email')}
+              type="email"
+              variant="outline"
+              className="mb-4"
+              error={t(errors?.email?.message!)}
+            />
+            <PasswordInput
+              label={t('form:input-label-password')}
+              forgotPassHelpText={t('form:input-forgot-password-label')}
+              {...register('password')}
+              error={t(errors?.password?.message!)}
+              variant="outline"
+              className="mb-4"
+              forgotPageLink={Routes.forgotPassword}
+            />
+            <Button className="w-full" loading={isLoading} disabled={isLoading}>
+              {t('form:button-label-login')}
+            </Button>
+          </>
+        )}
+      </Form>
+      {errorMessage ? (
+        <Alert
+          message={t(errorMessage)}
+          variant="error"
+          closeable={true}
+          className="mt-5"
+          onClose={() => setErrorMessage(null)}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default LoginForm;
+
+{
+  /* {errorMsg ? (
+          <Alert
+            message={t(errorMsg)}
+            variant="error"
+            closeable={true}
+            className="mt-5"
+            onClose={() => setErrorMsg('')}
+          />
+        ) : null} */
+}
